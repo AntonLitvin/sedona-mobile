22e7b9a732888c1df6b0d9177b3eedcf366553ff
Anton Litvinenko <anton8383@mail.ru>
2016-06-20 11:38:28 +0300

    add js code for open-close main-menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+var menuOpenBtn = document.querySelector(".main-nav-open");
+var menuCloseBtn = document.querySelector(".main-nav-close");
+var modalMenu = document.querySelector(".modal-menu");
+var mainNav = document.querySelector(".main-nav");
+
+menuOpenBtn.addEventListener("click", function(event) {
+	event.preventDefault();
+
+	if (modalMenu.classList.contains("modal-menu-open")) {
+		modalMenu.classList.remove("modal-menu-open");
+		mainNav.classList.remove("main-nav-active");
+	} else {
+	modalMenu.classList.add("modal-menu-open");
+	mainNav.classList.add("main-nav-active");
+	}
+
+});
+
+menuCloseBtn.addEventListener("click", function(event) {
+	event.preventDefault();
+
+	modalMenu.classList.remove("modal-menu-open");
+	mainNav.classList.remove("main-nav-active");
+	menuOpenBtn.classList.remove("active");
+
+});
+
+
 
 
 
