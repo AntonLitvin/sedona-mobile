886733323963cd5641b3c584138d269dceb89633
Anton Litvinenko <anton8383@mail.ru>
2016-06-27 12:23:40 +0300

    add styles for 1280px

--- js/script.js
+++ js/script.js
@@ -7,12 +7,16 @@ var menuCloseBtn = document.querySelector(".main-nav-close");
 var modalMenu = document.querySelector(".modal-menu");
 var mainNav = document.querySelector(".main-nav");
 
+function closeMenu() {
+	modalMenu.classList.remove("modal-menu-open");
+	mainNav.classList.remove("main-nav-active");
+}
+
 menuOpenBtn.addEventListener("click", function(event) {
 	event.preventDefault();
 
 	if (modalMenu.classList.contains("modal-menu-open")) {
-		modalMenu.classList.remove("modal-menu-open");
-		mainNav.classList.remove("main-nav-active");
+		closeMenu();
 	} else {
 		modalMenu.classList.add("modal-menu-open");
 		mainNav.classList.add("main-nav-active");
@@ -22,10 +26,7 @@ menuOpenBtn.addEventListener("click", function(event) {
 
 menuCloseBtn.addEventListener("click", function(event) {
 	event.preventDefault();
-
-	modalMenu.classList.remove("modal-menu-open");
-	mainNav.classList.remove("main-nav-active");
-
+	closeMenu();
 });
 
 })();
@@ -38,29 +39,28 @@ var link = document.querySelector(".search-hotel");
 var popup = document.querySelector(".search-hotel-modal");
 var close = popup.querySelector(".search-close");
 var submitClose = popup.querySelector(".submit-btn");
-
 var overlay = document.querySelector(".modal-overlay");
+
+function closeModal() {
+	popup.classList.remove("search-hotel-show");
+	overlay.classList.remove("modal-overlay-show");
+}
 
 link.addEventListener("click", function(event) {
 event.preventDefault();
-
 overlay.classList.add("modal-overlay-show");
 popup.classList.add("search-hotel-show");
-
 });
 
 close.addEventListener("click", function(event) {
 event.preventDefault();
-
-popup.classList.remove("search-hotel-show");
-overlay.classList.remove("modal-overlay-show");
+closeModal();
 });
 
 window.addEventListener("keydown", function(event) {
 	if (event.keyCode === 27) {
 		if (popup.classList.contains("search-hotel-show")) {
-			popup.classList.remove("search-hotel-show");
-			overlay.classList.remove("modal-overlay-show");
+			closeModal();
 		}
 	}
 });
@@ -68,19 +68,12 @@ window.addEventListener("keydown", function(event) {
 
 submitClose.addEventListener("click", function(event) {
 event.preventDefault();
-
-popup.classList.remove("search-hotel-show");
-overlay.classList.remove("modal-overlay-show");
-
+closeModal();
 });
-
 
 overlay.addEventListener("click", function(event) {
 event.preventDefault();
-
-popup.classList.remove("search-hotel-show");
-overlay.classList.remove("modal-overlay-show");
-
+closeModal();
 });
 
 })();
